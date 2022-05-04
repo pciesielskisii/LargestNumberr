b226faf7130ce00c99dd2738e0a03017ec06bf34
pawel <pciesielski@sii.pl>
2022-05-04 13:21:07 +0200

    add sort method

--- src/main/LargestNumber.java
+++ src/main/LargestNumber.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class LargestNumber {
+
+    public static void largestNumber(List<Integer> integerList) {
+            List<String> result = integerList
+                    .stream()
+                    .map(Object::toString).sorted((o1, o2) -> {
+                        String o1o2 = o1 + o2;
+
+                        String o2o1 = o2 + o1;
+
+                        return o1o2.compareTo(o2o1) > 0 ? -1 : 1;
+                    }).toList();
+            for (String s : result) System.out.print(s);
+
+    }
 }
