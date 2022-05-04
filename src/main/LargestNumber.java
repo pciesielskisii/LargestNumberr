d7da9c556772c711fc7674b15371d52f8d8f540a
pawel <pciesielski@sii.pl>
2022-05-04 13:21:50 +0200

    add validation

--- src/main/LargestNumber.java
+++ src/main/LargestNumber.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class LargestNumber {
 
     public static void largestNumber(List<Integer> integerList) {
+        if (!checkNegativeNumbers(integerList)) {
             List<String> result = integerList
                     .stream()
                     .map(Object::toString).sorted((o1, o2) -> {
@@ -13,6 +14,17 @@ public class LargestNumber {
                         return o1o2.compareTo(o2o1) > 0 ? -1 : 1;
                     }).toList();
             for (String s : result) System.out.print(s);
+        } else {
+            System.out.print("There are negative numbers");
+        }
+    }
 
+    public static Boolean checkNegativeNumbers(List<Integer> integerList) {
+        for (Integer integer : integerList) {
+            if (integer < 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
