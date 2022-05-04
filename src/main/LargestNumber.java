116dfa6629cf86d98498fe1e405c583fd399a1e8
pawel <pciesielski@sii.pl>
2022-05-04 15:42:02 +0200

    Add empty list validation

--- src/main/LargestNumber.java
+++ src/main/LargestNumber.java
@@ -1,9 +1,10 @@
+import java.util.Collection;
 import java.util.List;
 
 public class LargestNumber {
 
     public static void largestNumber(List<Integer> integerList) {
-        if (!checkNegativeNumbers(integerList)) {
+        if (largestNumberValidation(integerList)) {
             List<String> inputIntegerListAsString = integerList
                     .stream()
                     .map(Object::toString).sorted((o1, o2) -> {
@@ -14,17 +15,28 @@ public class LargestNumber {
                         return o1o2.compareTo(o2o1) > 0 ? -1 : 1;
                     }).toList();
             for (String s : inputIntegerListAsString) System.out.print(s);
-        } else {
-            System.out.print("There are negative numbers");
         }
     }
 
-    public static Boolean checkNegativeNumbers(List<Integer> integerList) {
+    private static Boolean checkNegativeNumbers(List<Integer> integerList) {
         for (Integer integer : integerList) {
             if (integer < 0) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static Boolean largestNumberValidation(List<Integer> integerList) {
+        boolean validated = true;
+        if (checkNegativeNumbers(integerList)) {
+            validated = false;
+            System.out.print("There are negative numbers");
+        }
+        if ((integerList.isEmpty())) {
+            validated = false;
+            System.out.print("List is empty");
+        }
+        return validated;
     }
 }
