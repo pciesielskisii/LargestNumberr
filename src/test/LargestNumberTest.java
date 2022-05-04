77472dc642c8c409ae558fa4284996123f3bb1d2
pawel <pciesielski@sii.pl>
2022-05-04 13:48:45 +0200

    Add largest number method test

--- src/test/LargestNumberTest.java
+++ src/test/LargestNumberTest.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LargestNumberTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void checkIfLargestNumberWorksCorrectly(){
+        List<Integer> list = Arrays.asList(1, 2 , 3);
+
+        LargestNumber.largestNumber(list);
+        assertEquals("321", outputStreamCaptor.toString().trim());
+    }
 }
